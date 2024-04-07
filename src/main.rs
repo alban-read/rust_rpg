54e7164f61b1573979f327f122734a4711a0288f
linatude <albanread@outlook.com>
2024-04-07 09:06:10 +0100

    experiments with terrain generation. Tiles are being created on demand, rather than stored in a map. Initialized Perlin noise only once.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@
 
 use std::sync::mpsc;
 use std::{io, thread};
+use std::cmp::max;
 use std::time::Duration;
 use std::fmt;
+use noise::{NoiseFn, Perlin};
 use std::mem::needs_drop;
-
+use image::{ImageBuffer, Rgb};
+use rayon::prelude::*;
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+use rand::Rng;
 
 // this is attempt at designing a simple role playing game in Rust
 
@@ -23,6 +29,157 @@ use std::mem::needs_drop;
 // They are a central feature of idiomatic, functional Rust code.
 // Use methods like map, filter, and fold to perform operations on game objects
 
+
+
+// ===========================================================================
+// Direction concepts
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Direction {
+    North,
+    South,
+    East,
+    West,
+    NorthEast,
+    NorthWest,
+    SouthEast,
+    SouthWest,
+}
+
+// direction offsets in terms of x,y
+impl Direction {
+    pub fn get_offset(&self) -> (i32, i32) {
+        match self {
+            Direction::North => (0, -1),
+            Direction::South => (0, 1),
+            Direction::East => (1, 0),
+            Direction::West => (-1, 0),
+            Direction::NorthEast => (1, -1),
+            Direction::NorthWest => (-1, -1),
+            Direction::SouthEast => (1, 1),
+            Direction::SouthWest => (-1, 1),
+        }
+    }
+
+    // return direction from x,y offset
+    pub fn from_offset(x: i32, y: i32) -> Option<Direction> {
+        match (x, y) {
+            (0, -1) => Some(Direction::North),
+            (0, 1) => Some(Direction::South),
+            (1, 0) => Some(Direction::East),
+            (-1, 0) => Some(Direction::West),
+            (1, -1) => Some(Direction::NorthEast),
+            (-1, -1) => Some(Direction::NorthWest),
+            (1, 1) => Some(Direction::SouthEast),
+            (-1, 1) => Some(Direction::SouthWest),
+            _ => None,
+        }
+    }
+
+
+    // Human readable direction names
+    pub fn name(&self) -> &str {
+        match self {
+            Direction::North => "North",
+            Direction::South => "South",
+            Direction::East => "East",
+            Direction::West => "West",
+            Direction::NorthEast => "NorthEast",
+            Direction::NorthWest => "NorthWest",
+            Direction::SouthEast => "SouthEast",
+            Direction::SouthWest => "SouthWest",
+        }
+    }
+
+    // Get the opposite direction
+    pub fn opposite(&self) -> Direction {
+        match self {
+            Direction::North => Direction::South,
+            Direction::South => Direction::North,
+            Direction::East => Direction::West,
+            Direction::West => Direction::East,
+            Direction::NorthEast => Direction::SouthWest,
+            Direction::NorthWest => Direction::SouthEast,
+            Direction::SouthEast => Direction::NorthWest,
+            Direction::SouthWest => Direction::NorthEast,
+        }
+    }
+
+    // turn right
+    pub fn turn_right(&self) -> Direction {
+        match self {
+            Direction::North => Direction::East,
+            Direction::South => Direction::West,
+            Direction::East => Direction::South,
+            Direction::West => Direction::North,
+            Direction::NorthEast => Direction::SouthEast,
+            Direction::NorthWest => Direction::NorthEast,
+            Direction::SouthEast => Direction::SouthWest,
+            Direction::SouthWest => Direction::NorthWest,
+        }
+    }
+
+    // turn left
+    pub fn turn_left(&self) -> Direction {
+        match self {
+            Direction::North => Direction::West,
+            Direction::South => Direction::East,
+            Direction::East => Direction::North,
+            Direction::West => Direction::South,
+            Direction::NorthEast => Direction::NorthWest,
+            Direction::NorthWest => Direction::SouthWest,
+            Direction::SouthEast => Direction::NorthEast,
+            Direction::SouthWest => Direction::SouthEast,
+        }
+    }
+
+    // get the direction from a string
+    pub fn from_str(s: &str) -> Option<Direction> {
+        match s {
+            "North" => Some(Direction::North),
+            "South" => Some(Direction::South),
+            "East" => Some(Direction::East),
+            "West" => Some(Direction::West),
+            "NorthEast" => Some(Direction::NorthEast),
+            "NorthWest" => Some(Direction::NorthWest),
+            "SouthEast" => Some(Direction::SouthEast),
+            "SouthWest" => Some(Direction::SouthWest),
+            _ => None,
+        }
+    }
+
+    pub fn from_lower_case_str(s: &str) -> Option<Direction> {
+        match s {
+            "north" => Some(Direction::North),
+            "south" => Some(Direction::South),
+            "east" => Some(Direction::East),
+            "west" => Some(Direction::West),
+            "northeast" => Some(Direction::NorthEast),
+            "northwest" => Some(Direction::NorthWest),
+            "southeast" => Some(Direction::SouthEast),
+            "southwest" => Some(Direction::SouthWest),
+            _ => None,
+        }
+    }
+
+
+    // random direction
+    pub fn random_direction() -> Direction {
+        let mut rng = rand::thread_rng();
+        match rng.gen_range(0..8) {
+            0 => Direction::North,
+            1 => Direction::South,
+            2 => Direction::East,
+            3 => Direction::West,
+            4 => Direction::NorthEast,
+            5 => Direction::NorthWest,
+            6 => Direction::SouthEast,
+            _ => Direction::SouthWest,
+        }
+    }
+}
+
+
 // implement the commands
 pub enum Command {
     Test,
@@ -30,6 +187,7 @@ pub enum Command {
     Quit,
     MeLook,
     Look(String),
+    Move,
     MoveTo(i32, i32),
     Attack,
     Defend,
@@ -40,8 +198,65 @@ pub enum Command {
 }
 
 
+
+
+static PERLIN: Lazy<Perlin> = Lazy::new(|| {
+    Perlin::new(7243)
+});
+
+// colours by name
+#[derive(Debug, Clone, Copy)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+impl Color {
+    pub const WHITE: Self = Self { r: 255, g: 255, b: 255 };
+    pub const SILVER: Self = Self { r: 192, g: 192, b: 192 };
+    pub const GRAY: Self = Self { r: 128, g: 128, b: 128 };
+    pub const BLACK: Self = Self { r: 0, g: 0, b: 0 };
+    pub const RED: Self = Self { r: 255, g: 0, b: 0 };
+    pub const MAROON: Self = Self { r: 128, g: 0, b: 0 };
+    pub const YELLOW: Self = Self { r: 255, g: 255, b: 0 };
+    pub const OLIVE: Self = Self { r: 128, g: 128, b: 0 };
+    pub const LIME: Self = Self { r: 0, g: 255, b: 0 };
+    pub const GREEN: Self = Self { r: 0, g: 128, b: 0 };
+    pub const AQUA: Self = Self { r: 0, g: 255, b: 255 };
+    pub const TEAL: Self = Self { r: 0, g: 128, b: 128 };
+    pub const BLUE: Self = Self { r: 0, g: 0, b: 255 };
+    pub const NAVY: Self = Self { r: 0, g: 0, b: 128 };
+    pub const FUCHSIA: Self = Self { r: 255, g: 0, b: 255 };
+    pub const PURPLE: Self = Self { r: 128, g: 0, b: 128 };
+}
+
+impl Color {
+    pub fn to_rgb(&self) -> image::Rgb<u8> {
+        image::Rgb([self.r, self.g, self.b])
+    }
+    pub fn lighter(&self) -> Self {
+        Self {
+            r: self.r.saturating_add(64),
+            g: self.g.saturating_add(64),
+            b: self.b.saturating_add(64),
+        }
+    }
+
+    pub fn darker(&self) -> Self {
+        Self {
+            r: self.r.saturating_sub(64),
+            g: self.g.saturating_sub(64),
+            b: self.b.saturating_sub(64),
+        }
+    }
+}
+
+
 pub trait GameObject {
     fn update(&self, world: &World) -> Self;
+    // return tile at x,y by creating new tile
+    fn get_tile(&self, x: i32, y: i32) -> Tile;
     // other common methods...
 }
 
@@ -54,9 +269,125 @@ pub struct Tile {
     // other tile properties...
 }
 
+// implement tile
+impl Tile {
+    // tile is generated by an algorithm, based on its position in the world.
+    pub fn new(x: i32, y: i32, width: i32, height: i32) -> Self {
+        let mut is_beach = false;
+
+        // if x and y are off the map, return limbo
+        if x < 0 || y < 0 || x >= width || y >= height {
+            return Tile {
+                terrain_type: TerrainType::Limbo,
+                x_position: x,
+                y_position: y,
+                elevation: 0,
+            };
+        }
+
+        // if the tile is within 4 tiles of the edge of the map, return boundary
+        if x < 4 || y < 4 || x >= width - 8 || y >= height - 8 {
+            return Tile {
+                terrain_type: TerrainType::Boundary,
+                x_position: x,
+                y_position: y,
+                elevation: 0,
+            };
+        }
+
+
+        let mut calculate_biased_elevation = |x: f64, y: f64, neighbors: &[(f64, f64)], map_width: f64, map_height: f64| -> f64 {
+            let elevation=Self::calc_elevation(width, height, x, y, map_width, map_height);
+            let mut sum_elevation = elevation;
+            let mut count = 1.0;
+            for (neighbor_x, neighbor_y) in neighbors {
+                let neighbor_elevation = Self::calc_elevation(width, height, *neighbor_x, *neighbor_y, map_width, map_height);
+                sum_elevation += neighbor_elevation;
+                count += 1.0;
+                // print elevation and neighbor elevation
+                // println!("Elevation: {}, Neighbor Elevation: {}", elevation, neighbor_elevation);
+                if elevation>0.0 && neighbor_elevation<1.0{
+                    is_beach=true;
+                }
+            }
+            let average_elevation = sum_elevation / count;
+            average_elevation
+        };
+
+
+        let mut calculate_elevation = |x: f64, y: f64, width: i32, height: i32| -> i32 {
+            let neighbors = [(x - 1.0, y), (x + 1.0, y), (x, y - 1.0), (x, y + 1.0)]; // left, right, up, down neighbors
+            let elevation = calculate_biased_elevation(x as f64, y as f64, &neighbors, width as f64, height as f64);
+            elevation as i32
+        };
+
+        let elevation = calculate_elevation(x.into(), y.into(), width, height);
+        let terrain_type = if elevation < 1 {
+            TerrainType::Water
+        } else if elevation >= 1 && is_beach {
+            //print!("Beach ");
+            TerrainType::Beach
+
+        } else if elevation < 100 {
+            TerrainType::Grass
+        } else {
+            TerrainType::Mountain
+        };
+
+        Tile {
+            terrain_type,
+            x_position: x,
+            y_position: y,
+            elevation,
+        }
+    }
+
+    // private static method to calculate elevation
+    fn calc_elevation(width: i32, height: i32, x: f64, y: f64, map_width: f64, map_height: f64) -> f64 {
+        let width_f64 = width as f64;
+        let height_f64 = height as f64;
+        let center_x = width_f64 / 2.0;
+        let center_y = height_f64 / 2.0;
+        let margin = 600.0;
+        let dx = x - center_x;
+        let dy = y - center_y;
+        let distance_to_center = (dx * dx + dy * dy).sqrt();
+        let max_distance = (width_f64 * width_f64 + height_f64 * height_f64).sqrt() - margin;
+        let normalized_distance = distance_to_center / max_distance;
+        let scale_factor = 60.0 * (1.0 - normalized_distance);
+        let noise_value = (&*PERLIN).get([x / map_width, y / map_height]);
+        let normalized_noise_value = (noise_value + 1.0) / 2.0; // Normalize to 0-1
+        let scaled_noise_value = normalized_noise_value * scale_factor;
+        scaled_noise_value - 20.0
+    }
+
+
+    pub fn execute_command(&self, command: Command) -> Self {
+        match command {
+            Command::MoveTo(x, y) => {
+                println!("Move to ({}, {})", x, y);
+                // Here you can add the logic to update the tile's position
+                Tile {
+                    x_position: x,
+                    y_position: y,
+                    ..(*self).clone()
+                }
+            }
+            Command::Idle => { (*self).clone() }
+            Command::Quit => { (*self).clone() }
+            _ => { (*self).clone() }
+        }
+    }
+}
+
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum TerrainType {
+    Limbo,
+    Boundary,
+    Earth,
     Grass,
+    Beach,
     Water,
     Mountain,
     // Add other terrain types here...
@@ -65,7 +396,6 @@ pub enum TerrainType {
 // derive clone
 #[derive(Clone, Debug, PartialEq)]
 pub struct GameMap {
-    tiles: Vec<Vec<Tile>>,
     width: i32,
     height: i32,
 }
@@ -78,50 +408,19 @@ impl GameObject for GameMap {
         println!("Game map updated");
         (*self).clone()
     }
+
+    // return tile at x,y by creating new tile
+    fn get_tile(&self, x: i32, y: i32) -> Tile {
+        Tile::new(x, y, self.width, self.height)
+    }
+
+
     // other common methods...
 }
 
 impl GameMap {
     pub fn new(width: i32, height: i32) -> Self {
-        fn calculate_elevation(x: i32, y: i32, width: i32, height: i32) -> i32 {
-            // Calculate the center of the map
-            let center_x = width / 2;
-            let center_y = height / 2;
-
-            // Calculate the distance of the tile from the center of the map
-            let dx = (x - center_x).abs();
-            let dy = (y - center_y).abs();
-
-            // Use the Pythagorean theorem to calculate the distance
-            let distance = ((dx * dx + dy * dy) as f64).sqrt();
-
-            // Calculate the maximum possible distance (from the center to a corner)
-            let max_distance = ((center_x * center_x + center_y * center_y) as f64).sqrt();
-
-            // Calculate the elevation based on the distance
-            let elevation = 75.0 * (1.0 - distance / max_distance);
-
-            // Return the elevation as an integer
-            elevation as i32
-        }
-
-        let mut tiles = Vec::new();
-        for y in 0..height {
-            let mut row = Vec::new();
-            for x in 0..width {
-                row.push(Tile {
-                    terrain_type: TerrainType::Grass,
-                    x_position: x,
-                    y_position: y,
-                    elevation: calculate_elevation(x, y, width, height),
-                });
-            }
-            tiles.push(row);
-        }
-
-
         GameMap {
-            tiles,
             width,
             height,
         }
@@ -140,6 +439,31 @@ impl GameMap {
             Command::Quit => { (*self).clone() }
             _ => { (*self).clone() }
         }
+    }
+
+
+    fn generate_map_image(&self, filename: &str) {
+
+        let width = self.width as u32;
+        let height = self.height as u32;
+        let mut img = ImageBuffer::<Rgb<u8>, _>::new(width, height);
+
+        img.enumerate_pixels_mut().par_bridge().for_each(|(x, y, pixel)| {
+            let tile = self.get_tile(x as i32, y as i32);
+            *pixel = match tile.terrain_type {
+                TerrainType::Water => Color::BLUE.to_rgb(),
+                TerrainType::Limbo => Color::FUCHSIA.to_rgb(),
+                TerrainType::Boundary => Color::WHITE.to_rgb(),
+                TerrainType::Earth | TerrainType::Grass | TerrainType::Mountain => {
+                    // generate a shade of green based on the elevation
+                    let elevation = tile.elevation as f32 / 100.0;
+                    let color = 100 -(elevation * 255.0) as u8;
+                    Rgb([0, color, 0])
+                }
+                TerrainType::Beach => Color::YELLOW.to_rgb(),
+            };
+        });
+        img.save(filename).unwrap();
     }
 }
 
@@ -219,10 +543,13 @@ impl Character {
             Command::Look(_) => {
                 (*self).clone()
             }
+            Command::Move => {
+
+
+                (*self).clone()}
         }
     }
 }
-
 
 impl fmt::Display for Character {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -231,12 +558,15 @@ impl fmt::Display for Character {
     }
 }
 
-
 impl GameObject for Character {
     fn update(&self, _world: &World) -> Self {
         // display the character's updated state
         println!("{} updated", self.name);
         (*self).clone()
+    }
+
+    fn get_tile(&self, x: i32, y: i32) -> Tile {
+        Tile::new(x, y, 2048, 2048)
     }
     // other common methods...
 }
@@ -302,6 +632,10 @@ impl GameObject for Items {
         println!("Items updated");
         (*self).clone()
     }
+
+    fn get_tile(&self, x: i32, y: i32) -> Tile {
+        Tile::new(x, y, 2048, 2048)
+    }
     // other common methods...
 }
 
@@ -320,6 +654,15 @@ impl GameObject for GameEntity {
             GameEntity::Character(character) => GameEntity::Character(character.update(world)),
             GameEntity::GameMap(game_map) => GameEntity::GameMap(game_map.update(world)),
             GameEntity::Items(items) => GameEntity::Items(items.update(world)),
+            // other game entities...
+        }
+    }
+
+    fn get_tile(&self, x: i32, y: i32) -> Tile {
+        match self {
+            GameEntity::Character(character) => character.get_tile(x, y),
+            GameEntity::GameMap(game_map) => game_map.get_tile(x, y),
+            GameEntity::Items(items) => items.get_tile(x, y),
             // other game entities...
         }
     }
@@ -475,9 +818,9 @@ impl World {
                 let character = self.search_for_named_character(name);
                 if let Some(character) = character {
                     // get the character's position
-                    let  x = character.x_position;
-                    let  y = character.y_position;
-                    println!("{:?}", self.game_map.tiles[x as usize][y as usize])
+                    let x = character.x_position;
+                    let y = character.y_position;
+                    println!("{:?}", character.get_tile(x, y))
                 } else {
                     println!("Character not found");
                 }
@@ -487,6 +830,7 @@ impl World {
                 // look around the player
                 println!("Look around player");
             }
+            Command::Move => {}
         }
         // After executing the command, update the world state
         *self = self.update();
@@ -664,6 +1008,8 @@ fn main() {
         world.add_item(item);
     }
 
+
+    world.game_map.generate_map_image("elevation_map.png");
 
     world.list_characters();
 
